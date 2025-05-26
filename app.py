e71fefab5037d8899868e769fa45e624fc5a3bbb
Suryansh Jain <129738369+suryanshjain95@users.noreply.github.com>
2025-05-26 12:36:36 +0000

    commit

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import streamlit as st
 import pandas as pd
 import yfinance as yf
 from datetime import date
 import time
 
-st.set_page_config(layout="wide")
 
 df = pd.read_csv('pages/data.csv')
 
@@ -32,49 +30,6 @@ def nm(n):
     x=df.iloc[n, 1]
     return str(x)
 
-def but(ticker):
-  if ticker:
-     st.session_state.ticker = ticker
-     st.switch_page("pages/plot.py")
-#with st.sidebar:
-#    st.image("icon.jpg",width=100)
+
 num_rows = len(df.index)
-
-for i in range(0,num_rows,4):
-
-    col1, col2, col3, col4 = st.columns(4, border=True)
-
-    a1,a2=change(df.iloc[i,2])
-    an="Details of "+df.iloc[i,1]
-    with col1:
-      if st.button(nm(i+1),key=df.iloc[i,2], type="tertiary"):
-         but(df.iloc[i,2])
-      st.metric(df.iloc[i,2],a1,a2[0:5])
-
-
-    b1,b2=change(df.iloc[i+1,2])
-    an="Details of "+df.iloc[i+1,1]
-    with col2:
-      if st.button(nm(i+2),key=df.iloc[i+1,2], type="tertiary"):
-         but(df.iloc[i+1,2])
-      st.metric(df.iloc[i+1,2],b1,b2[0:5])
-
-
-
-    c1,c2=change(df.iloc[i+2,2])
-    an="Details of "+df.iloc[i+2,1]
-    with col3:
-      if st.button(nm(i+3),key=df.iloc[i+2,2], type="tertiary"):
-         but(df.iloc[i+2,2])
-      st.metric(df.iloc[i+2,2],c1,c2[0:5])
-
-
-
-    d1,d2=change(df.iloc[i+3,2])
-    an="Details of "+df.iloc[i+3,1]
-    with col4:
-      if st.button(nm(i+4),key=df.iloc[i+3,2], type="tertiary"):
-         but(df.iloc[i+3,2])
-      st.metric(df.iloc[i+3,2],d1,d2[0:5])
-
 
